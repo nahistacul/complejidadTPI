b69fa6fe7e0b918eec9b04b225e6f5d84d6fc4c6
nahistacul <nahiarastacul@gmail.com>
2023-05-07 14:10:39 -0300

    Deorator

---
+++ main.py
@@ -0,0 +1,22 @@
+from classDef import Numero, Multiperfecto
+
+# def esMulti(suma, K, N):
+#     if suma==(K*N):
+#         return f'es multi'
+#     else:
+#         return f'NO es multi'
+
+
+if __name__ == "__main__":
+    N=Multiperfecto(int(input ('ingrese N= ')))
+    K=Numero(int(input ('ingrese K= ')))
+    print(N.valor)
+    print(K.valor)
+    N.setDivisores(N.calcularDivisores())
+    suma=N.sumarDivisores()
+
+    N.esMulti(suma,K.valor,N.valor)
+    
+
+
+    
